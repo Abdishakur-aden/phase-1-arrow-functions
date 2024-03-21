7c503f9adbd13a32ee0398bd0720df214108b532
Abdishakur Mukhtar <abdishakurmukhtar46@gmail.com>
2024-03-21 09:43:07 +0300

    complted lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide(){
+    let a = 2000;
+    let b = 100;
+    return a/b;
+}
+
+const square = number => {
+    let x = number**2;
+    return x;
+} 
+
+const add = (num1, num2) => {
+    let y = num1 + num2;
+    return y;
+}
